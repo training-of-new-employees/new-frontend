5f4ab3458a9b42ea72af4f57e5251ff2f9edd551
Kirill Karpov <rin.veber@gmail.com>
2024-02-07 23:42:22 +0400

    add fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import router from './App/Routes.ts';
+import router from './App/Routes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
