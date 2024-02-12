13f4358836b1b7ac2f7bf55afd83a12b6828d91d
YuliaDuk <yule444ka1234@mail.ru>
2024-02-13 00:24:18 +0300

    fix: hover на карточке

--- src/components/UI/DropDown/DropDown.tsx
+++ src/components/UI/DropDown/DropDown.tsx
@@ -6,7 +6,10 @@ export const DropDown: FC = () => {
     <div className="absolute top-[40px] pt-[6px] pb-[10px] bg-white right-0 w-[236px] rounded-[16px] shadow-[1px_1px_1.5px_1.5px_rgba(47,48,46,0.08)]">
       {CardMenu.map((i) => {
         return (
-          <button key={i.id} className="py-[7px] w-[100%] flex hover:bg-sidebarHoveredBtn">
+          <button
+            key={i.id}
+            className="py-[7px] w-[100%] flex hover:bg-sidebarHoveredBtn rounded-[16px]"
+          >
             <img className="px-[16px]" src={i.image} />
             <h4 className="text-h4 text-left">{i.text}</h4>
           </button>
