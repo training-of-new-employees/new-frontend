db017fd28689f12d58b5d96c8f437f397300ba16
Kirill Karpov <rin.veber@gmail.com>
2024-02-20 17:58:39 +0400

    add selector for store

--- src/store/RootStores.tsx
+++ src/store/RootStores.tsx
@@ -1,9 +1,14 @@
+import { makeAutoObservable } from 'mobx';
 import coursesStore from './courses-store';
 import profileStore from './profile-store';
 
 class RootStories {
   profileState = profileStore;
   coursesState = coursesStore;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
 }
 
 export default RootStories;
--- src/utils/context/root-context-store.ts
+++ src/utils/context/root-context-store.ts
@@ -3,12 +3,19 @@ import RootStories from '../../store/RootStores';
 
 export const RootStoriesContext = createContext<RootStories | null>(null);
 
-export const useStores = () => {
+export function useStores(): RootStories;
+export function useStores<Result>(selector: (value: RootStories) => Result): Result;
+
+export function useStores<Result>(selector?: (value: RootStories) => Result) {
   const context = useContext(RootStoriesContext);
 
   if (context === null) {
     throw Error('стор пустой');
   }
 
+  if (typeof selector === 'function') {
+    return selector(context);
+  }
+
   return context;
-};
+}
