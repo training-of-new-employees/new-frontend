9977aac2a6d2449df24bfb3e321b09fd3b4267b9
Kirill Karpov <rin.veber@gmail.com>
2024-02-20 16:30:08 +0400

    fix dev url

--- src/utils/constantsUrl.ts
+++ src/utils/constantsUrl.ts
@@ -14,20 +14,11 @@ const paths = {
 // pass: Ivan42_Maria
 // login: sir.jul@yandex.ru
 
-type UrlType = {
-  development: string;
-  production: string | undefined | NodeJS.Process;
-};
-
-const urlByEnvVar: UrlType = {
+const urlByEnvVar = {
   development: 'https://quickon.acceleratorpracticum.ru',
-  // development: 'http://localhost:8081', // generates by using command npm run start
-  production: process.env.REACT_APP_BASE_URL, // generates by using command npm run build
 };
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
-const baseUrl = `${urlByEnvVar[process.env.NODE_ENV || ''] || urlByEnvVar.development}/api/v1`;
+const baseUrl = `${urlByEnvVar.development}/api/v1`;
 const EMAIL_REGEX = '[a-z0-9]+@[a-z]+\\.{1,1}[a-z]{2,}';
 const NAME_REGEX = '[а-яА-Яa-zA-ZёË0-9\\- ]{1,}';
 
