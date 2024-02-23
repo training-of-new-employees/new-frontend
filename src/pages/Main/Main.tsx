bd3612cf2374ac6ebedc0bb22d2920e593d4422b
YuliaDuk <yule444ka1234@mail.ru>
2024-02-23 10:41:23 +0300

    feat: рыба для попапа

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,15 +1,13 @@
 import Body from './Body/Body';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
-import { Snackbar } from '../../components/UI/Snackbar/Snackbar';
 
 export default function Main() {
   return (
-    <Snackbar />
-    // <>
-    //   <Header />
-    //   <Body />
-    //   <Footer />
-    // </>
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
   );
 }
