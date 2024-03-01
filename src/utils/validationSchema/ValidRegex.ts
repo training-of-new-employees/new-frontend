bc8bed5f052a2c5b2aa71f0199a2f6ef47826676
Kirill Karpov <rin.veber@gmail.com>
2024-03-01 21:33:23 +0400

    добавил кириллицу в регулярку

--- src/utils/validationSchema/ValidRegex.ts
+++ src/utils/validationSchema/ValidRegex.ts
@@ -4,6 +4,6 @@ export const regexEmail =
   /^[a-zA-Z0-9!#$%&+\-*\/=?\^_{½~]+(?:\.[a-zA-Z0-9!#$%&+\-*\/=?\^_{½~]+)*@(?!-)[a-zA-Z0-9-]*(?:\.[a-zA-Z0-9]+)+$/;
 export const regexPassword =
   /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{4,}$/;
-export const regexName = /^[A-Za-z\s'-]+$/;
+export const regexName = /^[A-Za-zА-Яа-я0-9\s!$%&()+,-./:;<=>?@[\]^_{|}~]+$/;
 
-export const regexNameCourse = /^[A-Za-z0-9\s!$%&()+,-./:;<=>?@[\]^_`{|}~]+$/;
+export const regexNameCourse = /^[A-Za-zА-Яа-я0-9\s!$%&()+,-./:;<=>?@[\]^_`{|}~]+$/;
