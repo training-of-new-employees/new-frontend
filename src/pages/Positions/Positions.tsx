7c6950e60ab7b518eea7fb4b7853888c20c75bc4
YuliaDuk <yule444ka1234@mail.ru>
2024-03-11 10:46:11 +0300

    feat: страница Должности

--- src/pages/Positions/Positions.tsx
+++ src/pages/Positions/Positions.tsx
@@ -1,3 +1,18 @@
-export default function Positions() {
-  return <div>Positions</div>;
-}
+import { FC } from 'react';
+import { CardGrid } from '../../components/CardGrid/CardGrid';
+import Button from '../../components/UI/Button/Button';
+
+export const Positions: FC = () => {
+  return (
+    <section className="pt-[20px] px-[20px] w-[100%]">
+      <div className="max-w-[260px]">
+        <Button type="button" variant="primary" icon="white">
+          Новая должность
+        </Button>
+      </div>
+      <div className="pt-[20px]">
+        <CardGrid mode="role" />
+      </div>
+    </section>
+  );
+};
