c70f7afa52fdf3ff27db3bc6e8d55c0c88799578
YuliaDuk <yule444ka1234@mail.ru>
2024-03-15 00:53:14 +0300

    feat: добавлено пустое состояние для должности

--- src/pages/Positions/Positions.tsx
+++ src/pages/Positions/Positions.tsx
@@ -1,16 +1,24 @@
 import { FC } from 'react';
 import { CardGrid } from '../../components/CardGrid/CardGrid';
+import { NoPositions } from '../../components/NoContent';
 import Button from '../../components/UI/Button/Button';
 import { POSITIONS_DATA } from '../../utils/constants';
 
 export const Positions: FC = () => {
+  const isAdmin = localStorage.getItem('role') === 'ADMIN';
   return (
     <section className="pt-[20px] px-[20px] w-[100%]">
-      <div className="max-w-[260px]">
-        <Button type="button" variant="primary" icon="white">
-          Новая должность
-        </Button>
-      </div>
+      {isAdmin && POSITIONS_DATA.length === 0 ? (
+        <NoPositions />
+      ) : isAdmin ? (
+        <div className="max-w-[260px]">
+          <Button type="button" variant="primary" icon="white">
+            Новая должность
+          </Button>
+        </div>
+      ) : (
+        ''
+      )}
       <div className="pt-[20px]">
         <CardGrid data={POSITIONS_DATA} mode="role" />
       </div>
