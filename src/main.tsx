d29bbddea101df456e15c5ec73bec7b6fb1e33e5
Kirill Karpov <rin.veber@gmail.com>
2024-03-15 12:57:21 +0400

    fix eslint

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,8 @@ import { RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import router from './App/Routes';
-// eslint-disable-next-line import/order
-import { spy } from 'mobx';
 import RootStories from './store/RootStores';
 import { RootStoriesContext } from './utils/context/root-context-store';
-
-// spy((event) => {
-//   if (event.type == 'action') {
-//     console.log(event);
-//   }
-// });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
