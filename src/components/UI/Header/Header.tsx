5e5e67435b117e9f924bc9067da948cfcb6d2172
YuliaDuk <yule444ka1234@mail.ru>
2024-03-17 21:03:39 +0300

    feat: попап выхода открывается при нажатии на кнопку в хедере

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -1,7 +1,9 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { PopupExitAccount } from '../../PopupExitAccount/PopupExitAccount';
 
 export const Header: FC = () => {
+  const [isPopupOpened, setPopupOpened] = useState(false);
   return (
     <header className="w-[100%] relative bg-fontColor flex justify-between border-b-[4px] border-defaultBtn p-[10px]">
       <NavLink
@@ -9,9 +11,20 @@ export const Header: FC = () => {
         className="bg-logo-header  min-h-[48px] min-w-[134px] bg-center bg-no-repeat ml-[10px]"
       />
       <div className="flex gap-[10px]">
-        <button className="bg-profile-avatar bg-center bg-no-repeat min-w-[48px] h-[48px] bg-[length:48px_48px] mr-[10px] border-white border-[2px] rounded-[50%]" />
-        <button className="bg-profile-exitbtn bg-center bg-no-repeat min-w-[48px] h-[48px] bg-[length:48px_48px] mr-[10px]" />
+        <NavLink
+          to="/profile"
+          className="bg-profile-avatar bg-center bg-no-repeat min-w-[48px] h-[48px] bg-[length:48px_48px] mr-[10px] border-white border-[2px] rounded-[50%]"
+        />
+        <button
+          onClick={() => setPopupOpened(true)}
+          className="bg-profile-exitbtn bg-center bg-no-repeat min-w-[48px] h-[48px] bg-[length:48px_48px] mr-[10px]"
+        />
       </div>
+      {isPopupOpened ? (
+        <PopupExitAccount setIsOpened={setPopupOpened} isOpened={isPopupOpened} />
+      ) : (
+        ''
+      )}
     </header>
   );
 };
