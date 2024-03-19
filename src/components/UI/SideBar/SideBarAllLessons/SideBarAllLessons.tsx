16848e68e2055d5ef6e5d97a706c23867db0e61d
YuliaDuk <yule444ka1234@mail.ru>
2024-03-19 10:57:43 +0300

    feat: кастомный скроллбар

--- src/components/UI/SideBar/SideBarAllLessons/SideBarAllLessons.tsx
+++ src/components/UI/SideBar/SideBarAllLessons/SideBarAllLessons.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { useNavigate, NavLink, useLocation } from 'react-router-dom';
+import { useNavigate, NavLink } from 'react-router-dom';
 import Button from '../../Button/Button';
 
 export const SideBarAllLessons: FC = () => {
