e827ce28c986f8c3defc6a0d3675e65f27ad91ed
YuliaDuk <yule444ka1234@mail.ru>
2024-03-21 10:45:25 +0300

    fix: отредактирован отступ слева у wrapper

--- src/components/UI/WrapperForAddForm/WrapperForAddForm.tsx
+++ src/components/UI/WrapperForAddForm/WrapperForAddForm.tsx
@@ -2,5 +2,5 @@ import { FC } from 'react';
 import { IWrapperForAddForm } from './WrapperForAddFormTypes';
 
 export const WrapperForAddForm: FC<IWrapperForAddForm> = ({ children }) => {
-  return <div className="pt-[72px] flex flex-col items-center w-full ">{children}</div>;
+  return <div className="pt-[72px] flex flex-col items-center w-full ml-[300px]">{children}</div>;
 };
