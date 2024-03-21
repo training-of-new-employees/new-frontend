4e734a1e34e5a057dee58e6b9c2c91362513768e
Kirill Karpov <rin.veber@gmail.com>
2024-03-21 13:19:29 +0400

    fix error

--- src/components/CardGrid/CardGrid.tsx
+++ src/components/CardGrid/CardGrid.tsx
@@ -1,38 +1,50 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ICardGrid } from './CardGridTypes';
 import { Card } from '../UI/Card/Card';
 
 export const CardGrid: FC<ICardGrid> = ({ mode, data }) => {
   const navigate = useNavigate();
-  return (
-    <div className="grid gap-[15px] grid-cols-auto-fit pb-[20px]">
-      {mode === 'course'
-        ? Array.from(data).map((i) => {
-            return (
-              <Card
-                id={i.id}
-                key={i.id}
-                name={i.name}
-                mode={mode}
-                lesson={1}
-                status={'Не начат'}
-                onClick={() => navigate(`/courses/${i.id}`)}
-              />
-            );
-          })
-        : Array.from(data).map((i) => {
-            return (
-              <Card
-                id={i.id}
-                key={i.id}
-                name={i.name}
-                mode={mode}
-                courses={i.courses}
-                personal={i.personal}
-              />
-            );
-          })}
-    </div>
-  );
+  const [isLoading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    if (data) {
+      setLoading(true);
+    }
+  }, [data]);
+
+  if (isLoading == false) {
+    return null;
+  } else {
+    return (
+      <div className="grid gap-[15px] grid-cols-auto-fit pb-[20px]">
+        {mode === 'course'
+          ? Array.from(data).map((i) => {
+              return (
+                <Card
+                  id={i.id}
+                  key={i.id}
+                  name={i.name}
+                  mode={mode}
+                  lesson={1}
+                  status={'Не начат'}
+                  onClick={() => navigate(`/courses/${i.id}`)}
+                />
+              );
+            })
+          : Array.from(data).map((i) => {
+              return (
+                <Card
+                  id={i.id}
+                  key={i.id}
+                  name={i.name}
+                  mode={mode}
+                  courses={i.courses}
+                  personal={i.personal}
+                />
+              );
+            })}
+      </div>
+    );
+  }
 };
