5495fa0083d497b656775021166887a45aab0faf
Kirill Karpov <rin.veber@gmail.com>
2024-03-25 20:38:46 +0400

    fix getToken

--- src/utils/tokenActions/getToken.tsx
+++ src/utils/tokenActions/getToken.tsx
@@ -1,12 +1,4 @@
 export const getToken = (): string | null => {
-  let result = null;
-
-  const storedToken = localStorage.getItem('tokenKey');
-  // storedToken && (result = JSON.parse(storedToken))
-  result = storedToken;
-
-
-export const getToken = (): string | null => {
   const token =
     localStorage.getItem('tick') === 'true'
       ? localStorage?.getItem('token')
